b57c83791801d6323516c03dfafd66a4b825c6ca
matheus.martinez <matheus.martinez@uppertools.inf.br>
2021-07-30 08:04:39 -0300

    Implementação autenticação social GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,23 @@
 import { FaGithub } from 'react-icons/fa';
 import styles from './styles.module.scss';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Matheus Martinez
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn()}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
